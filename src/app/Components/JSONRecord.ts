e5e60f8d2ccb314050e4ca04f6b26c1afa845a81
LaraAngel <lara.angel.js@gmail.com>
2022-09-08 23:46:53 -0500

    JSON records

--- src/app/Components/JSONRecord.ts
+++ src/app/Components/JSONRecord.ts
@@ -1,8 +1,20 @@
 export class JSONRecord {
-  public id: any;
   public title: any;
   public code: any;
   public sources: any;
-  public inPutData: any;
-  public outPutData: any;
+  public inPutData: InputData = new InputData;
+  public outPutData: OutputData = new OutputData;
+  public data: Data[] = [];
+}
+export class Data{
+  public subtitle: any;
+  public body: any;
+  public image: any;
+}
+export class InputData{
+  public images: string[] = [];
+}
+
+export class OutputData{
+  public images: string[] = [];
 }
